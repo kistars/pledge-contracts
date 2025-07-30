01989fc7dba57192c6272942234f94417bed953d
kistars <sunllen2014@outlook.com>
2025-07-30 11:27:49 +0800

    fix: modify scripts

--- tests/helper/init.js
+++ tests/helper/init.js
@@ -1,11 +1,12 @@
-const BN = web3.utils.BN;
-const zeroAddress = "0x0000000000000000000000000000000000000000"
+// const BN = web3.utils.BN;
+// const zeroAddress = "0x0000000000000000000000000000000000000000"
 
 async function initFactory(minter) {
     // factory
     const Factory = await ethers.getContractFactory("UniswapV2Factory");
     // set feeTo setter to minter
-    factory = await Factory.deploy(minter.address)
+    factory = await Factory.deploy(minter.address);
+    await factory.waitForDeployment();
     return factory;
 }
 
@@ -13,27 +14,31 @@ async function initFactory(minter) {
 async function initWETH() {
     // @Notice Mock WETH, will be replaced in formal deploy
     const WETH = await ethers.getContractFactory("WETH9");
-    weth = await WETH.deploy()
+    weth = await WETH.deploy();
+    await weth.waitForDeployment();
     return weth;
 }
 
 
 async function initRouter(factory, weth) {
     const Rounter = await ethers.getContractFactory("UniswapV2Router02");
-    router = await Rounter.deploy(factory.address, weth.address)
+    router = await Rounter.deploy(factory.target, weth.target);
+    await router.waitForDeployment();
     return router
 }
 
 
-async function initBusd(){
-    const Busd = await  ethers.getContractFactory("BEP20Token");
+async function initBusd() {
+    const Busd = await ethers.getContractFactory("BEP20Token");
     busd = await Busd.deploy();
+    await busd.waitForDeployment();
     return busd
 }
 
 async function initBtc() {
-    const Btc= await ethers.getContractFactory("BtcToken");
+    const Btc = await ethers.getContractFactory("BtcToken");
     btc = await Btc.deploy();
+    await btc.waitForDeployment();
     return btc
 }
 
@@ -45,7 +50,7 @@ async function initAll(minter) {
     let router = await initRouter(factory, weth);
 
     let busd = await initBusd();
-    let btc = await initBtc()
+    let btc = await initBtc();
 
     return [weth, factory, router, busd, btc]
 }
--- tests/helper/time.js
+++ tests/helper/time.js
@@ -1,47 +1,46 @@
 
-const BN = web3.utils.BN;
-async function latestBlock () {
-  const block = await web3.eth.getBlock('latest');
-  return new BN(block.number);
+const { ethers } = require("hardhat");
+
+async function latestBlock() {
+  const block = await ethers.provider.getBlock('latest');
+  return ethers.BigNumber.from(block.number);
 }
 
-async function latestBlockNum () {
-  const block = await web3.eth.getBlock('latest');
-  return (new BN(block.number)).toNumber();
+async function latestBlockNum() {
+  const block = await ethers.provider.getBlock('latest');
+  return block.number;
 }
 
+async function showBlock() {
+  const block = await ethers.provider.getBlock('latest');
+  console.log("Block number: " + block.number.toString());
+}
 
-async function showBlock () {
-    const block = await web3.eth.getBlock('latest');
-    console.log("Block number: " + new BN(block.number).toString());
-  }
-
-async function showBlock (msg) {
-    const block = await web3.eth.getBlock('latest');
-    console.log(msg + " at block number: " + new BN(block.number).toString());
-  }
+async function showBlock(msg) {
+  const block = await ethers.provider.getBlock('latest');
+  console.log(msg + " at block number: " + block.number.toString());
+}
 
 async function stopAutoMine() {
-    //stop auto mine or it will mess the block number
-    network.provider.send("evm_setIntervalMining", [600000])
-    // await network.provider.send("evm_setAutomine", [false])
+  //stop auto mine or it will mess the block number
+  await ethers.provider.send("evm_setIntervalMining", [600000])
+  // await ethers.provider.send("evm_setAutomine", [false])
 }
 
-function advanceBlock () {
-  // return network.provider.send("evm_mine", [new Date().getTime()])
-  return network.provider.send("evm_mine", [])
+function advanceBlock() {
+  // return ethers.provider.send("evm_mine", [new Date().getTime()])
+  return ethers.provider.send("evm_mine", [])
 }
-
 
 // Advance the block to the passed height
-async function advanceBlockTo (target) {
-    // stop interval mint,set to 600s
+async function advanceBlockTo(target) {
+  // stop interval mint,set to 600s
   await stopAutoMine()
-  if (!BN.isBN(target)) {
-    target = new BN(target);
+  if (!ethers.BigNumber.isBigNumber(target)) {
+    target = ethers.BigNumber.from(target);
   }
 
-  const currentBlock = (await latestBlock());
+  const currentBlock = await latestBlock();
   const start = Date.now();
   let notified;
   if (target.lt(currentBlock)) throw Error(`Target block #(${target}) is lower than current block #(${currentBlock})`);
@@ -49,8 +48,8 @@ async function advanceBlockTo (target) {
     if (!notified && Date.now() - start >= 5000) {
       notified = true;
       console.log(`\
-${colors.white.bgBlack('@openzeppelin/test-helpers')} ${colors.black.bgYellow('WARN')} advanceBlockTo: Advancing too ` +
-      'many blocks is causing this test to be slow.');
+@openzeppelin/test-helpers WARN advanceBlockTo: Advancing too ` +
+        'many blocks is causing this test to be slow.');
     }
     await advanceBlock();
   }
@@ -58,24 +57,23 @@ ${colors.white.bgBlack('@openzeppelin/test-helpers')} ${colors.black.bgYellow('W
 }
 
 // Returns the time of the last mined block in seconds
-async function latest () {
-    const block = await web3.eth.getBlock('latest');
-    return new BN(block.timestamp);
-  }
+async function latest() {
+  const block = await ethers.provider.getBlock('latest');
+  return ethers.BigNumber.from(block.timestamp);
+}
 
 async function increase(seconds) {
-  await network.provider.send("evm_increaseTime", [seconds])
+  await ethers.provider.send("evm_increaseTime", [seconds])
   await advanceBlock();
 }
 
-
 module.exports = {
-    advanceBlockTo,
-    advanceBlock,
-    latestBlock,
-    latestBlockNum,
-    showBlock,
-    stopAutoMine,
-    latest,
-    increase
+  advanceBlockTo,
+  advanceBlock,
+  latestBlock,
+  latestBlockNum,
+  showBlock,
+  stopAutoMine,
+  latest,
+  increase
 };
