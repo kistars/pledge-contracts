eadce78e88ddc98d2c1b47eaef4f40b67a0cd383
kistars <sunllen2014@outlook.com>
2025-07-30 10:35:13 +0800

    fix: compile error

--- contracts/mock/MockUniswap/UniswapV2Factory.sol
+++ contracts/mock/MockUniswap/UniswapV2Factory.sol
@@ -22,26 +22,26 @@ interface IUniswapV2Factory {
 }
 
 interface IUniswapV2Pair {
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    // event Approval(address indexed owner, address indexed spender, uint256 value);
+    // event Transfer(address indexed from, address indexed to, uint256 value);
 
-    function name() external pure returns (string memory);
-    function symbol() external pure returns (string memory);
-    function decimals() external pure returns (uint8);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address owner) external view returns (uint256);
-    function allowance(address owner, address spender) external view returns (uint256);
+    // function name() external pure returns (string memory);
+    // function symbol() external pure returns (string memory);
+    // function decimals() external pure returns (uint8);
+    // function totalSupply() external view returns (uint256);
+    // function balanceOf(address owner) external view returns (uint256);
+    // function allowance(address owner, address spender) external view returns (uint256);
 
-    function approve(address spender, uint256 value) external returns (bool);
-    function transfer(address to, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    // function approve(address spender, uint256 value) external returns (bool);
+    // function transfer(address to, uint256 value) external returns (bool);
+    // function transferFrom(address from, address to, uint256 value) external returns (bool);
 
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-    function PERMIT_TYPEHASH() external pure returns (bytes32);
-    function nonces(address owner) external view returns (uint256);
+    // function DOMAIN_SEPARATOR() external view returns (bytes32);
+    // function PERMIT_TYPEHASH() external pure returns (bytes32);
+    // function nonces(address owner) external view returns (uint256);
 
-    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
-        external;
+    // function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+    //     external;
 
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
     event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
